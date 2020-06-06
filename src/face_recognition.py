29023e019d303a0aa8cf79276a2ef93821f9b041
Poly <poly.wave@yandex.ru>
2020-06-06 17:53:39 +0400

    Fix

--- src/face_recognition.py
+++ src/face_recognition.py
@@ -11,7 +11,7 @@ from mq import Mq
 class FaceRecognition:
     def __init__(self):
         self.matches = 1
-        self.confidence = 60
+        self.confidence = 70
 
         # Init operations
         self.mq = Mq()
