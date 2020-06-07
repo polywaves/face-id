eb496b012a8224b0680758af8c7cd83b0ea3b623
Poly <poly.wave@yandex.ru>
2020-06-07 19:03:44 +0400

    Fix

--- src/classifier.py
+++ src/classifier.py
@@ -22,7 +22,7 @@ class Classifier:
         self.grab_faces = 300
         self.use_faces = 300
         self.min_faces = 200
-        self.thresh = 0.5
+        self.thresh = 0.2
         self.thresh_vgg = 0.08
 
         self.dnn_picture_size_x = 96
